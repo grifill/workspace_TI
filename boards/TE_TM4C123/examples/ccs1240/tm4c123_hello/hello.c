35fb8c9fa9040c30342c1f9d90e2212501a357a3
Grigorii Filatov <grigorijfilatov@gmail.com>
2026-01-24 23:00:41 +0300

    Hello Done
    TM4C123 Tiva-C Hello world proj done

--- boards/TE_TM4C123/examples/ccs1240/tm4c123_hello/hello.c
+++ boards/TE_TM4C123/examples/ccs1240/tm4c123_hello/hello.c
@@ -20,8 +20,60 @@
  */
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+//*****************************************************************************
+// The error routine that is called if the driver library encounters an error.
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count) {
+    while(ui32Count--) {
+        MAP_UARTCharPut(UART0_BASE, *pui8Buffer++);
+    }
+}
 
 int main(void) {
-    printf("Hello World!\n");
-    return 0;
+
+    // Set the clocking to run directly from the crystal
+    MAP_SysCtlClockSet(SYSCTL_SYSDIV_1 |
+                       SYSCTL_USE_OSC |
+                       SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    // Enable the peripherals used by this example
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    // Set GPIO A0 and A1 as UART pins
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    MAP_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Configure the UART for 115,200, 8-N-1 operation
+    MAP_UARTConfigSetExpClk(UART0_BASE, MAP_SysCtlClockGet(), 115200,
+                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    // Prompt for text to be entered
+    UARTSend("\r\n Hello TE-TM4C123 \n", 21);
+
+    while(1) {
+    }
+
+    //return 0;
 }
