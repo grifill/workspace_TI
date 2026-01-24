4b9417548064a84708d02e4a9dfbeb2329aaf936
Grigorii Filatov <grigorijfilatov@gmail.com>
2026-01-24 21:27:18 +0300

    add descriptions

--- boards/TE_TM4C123/examples/ccs1240/tm4c123_hello/hello.c
+++ boards/TE_TM4C123/examples/ccs1240/tm4c123_hello/hello.c
@@ -1,12 +1,27 @@
+/**
+ * ---------------------------------------------------------------------------
+ * Copyright (c) 2026 Gregory Filatov
+ * All rights reserved.
+ *
+ * \file   hello.c
+ * \author Gregory Filatov
+ * \date   24 January 2026
+ * \version 0.1.0
+ *
+ * \brief  Contains hello example for TM4C123GH6PZ chip
+ *
+ * Module: Simple example
+ *
+ * Purpose: Example, education
+ *
+ * Functionality:
+ *     Environment
+ * ---------------------------------------------------------------------------
+ */
+#include <stdint.h>
 #include <stdio.h>
 
-
-/**
- * hello.c
- */
-int main(void)
-{
-	printf("Hello World!\n");
-	
-	return 0;
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
 }
