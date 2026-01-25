eac8e2b4c54c6a68f07f032813a8d20480169ca3
Grigorii Filatov <grigorijfilatov@gmail.com>
2026-01-25 17:33:10 +0300

    TIVA library location

--- boards/TE_TM4C123/examples/uart_hello/uart_hello.c
+++ boards/TE_TM4C123/examples/uart_hello/uart_hello.c
@@ -3,12 +3,12 @@
  * Copyright (c) 2026 Gregory Filatov
  * All rights reserved.
  *
- * \file   hello.c
+ * \file   uart_hello.c
  * \author Gregory Filatov
  * \date   24 January 2026
  * \version 0.1.0
  *
- * \brief  Contains hello example for TM4C123GH6PZ chip
+ * \brief  Contains UART-Hello Example for TM4C123GH6PZ chip
  *
  * Module: Simple example
  *
