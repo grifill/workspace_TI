e0900027b650c33330011d35d0b013119f0ebe69
Grigorii Filatov <grigorijfilatov@gmail.com>
2026-01-28 21:39:39 +0300

    Add timer example

---
+++ boards/TE_TM4C123/examples/timer_leds/timer_leds.c
@@ -0,0 +1,60 @@
+/**
+ * ---------------------------------------------------------------------------
+ * Copyright (c) 2026 Gregory Filatov
+ * All rights reserved.
+ *
+ * \file   timer_leds.c
+ * \author Gregory Filatov
+ * \date   28 January 2026
+ * \version 0.1.0
+ *
+ * \brief  Contains Timer Example for TM4C123GH6PZ chip
+ *
+ * Module: Simple example
+ *
+ * Purpose: Example, education
+ *
+ * Functionality:
+ *     Environment
+ * ---------------------------------------------------------------------------
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+
+//*****************************************************************************
+// The error routine that is called if the driver library encounters an error.
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+
+int main(void) {
+
+    // Set the clocking to run directly from the crystal
+    MAP_SysCtlClockSet(SYSCTL_SYSDIV_1 |
+                       SYSCTL_USE_OSC |
+                       SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    // Enable the peripherals used by this example
+
+    while(1) {
+    }
+
+    //return 0;
+}
