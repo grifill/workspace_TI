f5b16937c8ec6af34c1041d845a31648b491c602
Grigorii Filatov <grigorijfilatov@gmail.com>
2026-02-16 21:44:15 +0300

    Add Custom TI libs

---
+++ boards/TE_TM4C123/libs/Custom_TI_Libs/func_gpio.h
@@ -0,0 +1,28 @@
+#ifndef FUNC_GPIO_H_
+#define FUNC_GPIO_H_
+
+//*****************************************************************************
+//
+// If building with a C++ compiler, make all of the definitions in this header
+// have a C binding.
+//
+//*****************************************************************************
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+//*****************************************************************************
+//
+// Mark the end of the C bindings section for C++ compilers.
+//
+//*****************************************************************************
+#ifdef __cplusplus
+}
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#endif // FUNC_GPIO_H_
